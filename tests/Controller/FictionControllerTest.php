3219cf1182f4b02d6ed406b4d144e94831c51d1e
gdarquie <gaetan.darquie@gmail.com>
2018-04-10 14:00:15 +0200

    Travail sur les tests fictions, postFictionWithTextes ok

--- tests/Controller/FictionControllerTest.php
+++ tests/Controller/FictionControllerTest.php
@@ -6,7 +6,7 @@ use App\Tests\ApiTestCase;
 
 class FictionControllerTest extends ApiTestCase
 {
-    public function testPostFiction()
+    public function testPostFiction()  //pas possible sans textes?
     {
         $data = array(
             'titre' => 'Nouvel exemple de titre de fiction',
@@ -19,6 +19,7 @@ class FictionControllerTest extends ApiTestCase
         ]);
 
         $this->assertEquals(201, $response->getStatusCode());
+
         $this->assertTrue($response->hasHeader('Location'));
 
         $fictionUrl = $response->getHeader('Location');
@@ -33,21 +34,42 @@ class FictionControllerTest extends ApiTestCase
 
     }
 
-    public function testPostFictionWithTextes(){
+    public function testPostFictionWithTextes() // fonction ok!!
+    {
         $data = array(
             'titre' => 'Nouvel exemple de titre de fiction',
-            'description' => 'Une description de fiction comme exemple',
-            'promesse' => 'Un contenu de promesse'
+            'description' => 'Une description de fiction comme exemple'
         );
-        $textes = [];
-        $data = array_push($data, $textes);
+
+        $data['textes'][0]['titre'] = 'Test 1';
+        $data['textes'][0]['description'] = 'Description 1';
+        $data['textes'][0]['type'] = 'Contenu de la promesse';
+
+
+        $response = $this->client->post('/fictions', [
+            'body' => json_encode($data)
+        ]);
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        $this->assertTrue($response->hasHeader('Location'));
+        $fictionUrl = $response->getHeader('Location');
+
+        $response = $this->client->get($fictionUrl[0]);
+
+        $finishedData = json_decode($response->getBody(true), true);
+        $this->assertArrayHasKey('titre', $finishedData, "Il n'y a pas de champ titre");
+
+        echo $response->getBody();
+        echo "\n\n";
     }
 
     public function testGetFiction()
     {
-        $fictionId = 1;
 
         //createFiction and get its id
+        $fictionId = 1; //and replace this line
+
 
         $response = $this->client->get('/fictions/'.$fictionId);
         $this->assertEquals(200, $response->getStatusCode());
