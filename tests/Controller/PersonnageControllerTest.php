ccc5c812cf5e37ffd4a5ff4bfd4c2880dd51d751
gdarquie <gaetan.darquie@gmail.com>
2018-04-24 14:00:32 +0200

    Test postPersonnage

--- tests/Controller/PersonnageControllerTest.php
+++ tests/Controller/PersonnageControllerTest.php
@@ -1,19 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gaetan
- * Date: 09/04/2018
- * Time: 13:52
- */
 
 namespace App\tests\Controller;
 
+use App\Tests\ApiTestCase;
 
-class PersonnageControllerTest
+class PersonnageControllerTest extends ApiTestCase
 {
     public function testPostPersonnage()
     {
+        $data = array(
+            "titre" => "Barius",
+            "description" => "Le Sage",
+            "discriminateur" => "personnage",
+            "annee_naissance" => 0,
+            "annee_mort" => 120
+        );
 
+        $response = $this->client->post(ApiTestCase::TEST_PREFIX.'/personnages', [
+            'body' => json_encode($data)
+        ]);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertTrue($response->hasHeader('Location'));
+
+        $fictionUrl = $response->getHeader('Location');
+        $response = $this->client->get($fictionUrl[0]);
+
+        $finishedData = json_decode($response->getBody(true), true);
+        $this->assertArrayHasKey('titres', $finishedData, "Il n'y a pas de champ titre");
+
+        echo $response->getBody();
+        echo "\n\n";
     }
 
     public function testGetPersonnage()
