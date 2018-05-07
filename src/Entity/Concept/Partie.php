79e298088135df9a8a491e8e93f6d63bc6a3cbcd
gdarquie <gaetan.darquie@gmail.com>
2018-05-07 23:41:35 +0200

    Création entité Partie

--- src/Entity/Concept/Partie.php
+++ src/Entity/Concept/Partie.php
@@ -1,6 +1,24 @@
 <?php
 
-class Partie
-{
+namespace App\Entity\Concept;
 
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Modele\AbstractConcept;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="partie")
+ */
+class Partie extends AbstractConcept
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Concept\Partie")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", nullable=true)
+     */
+    private $parent;
+
+    /**
+     * @var integer
+     */
+    private $niveau;
 }
