406f63a3fe703ade971f5ad5c26d0999453679e0
gdarquie <gaetan.darquie@gmail.com>
2018-09-16 14:55:09 +0200

    MI-66: entité Lieu

---
+++ src/Entity/Element/Lieu.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity\Element;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Modele\AbstractItem;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="lieu")
+ */
+class Lieu extends AbstractItem
+{
+    /**
+    * @ORM\Column(type="decimal", scale=8)
+    **/
+    protected $lat;
+
+    /**
+     * @ORM\Column(type="decimal", scale=8)
+     **/
+    protected $long;
+
+    /**
+     * @return mixed
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param mixed $lat
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLong()
+    {
+        return $this->long;
+    }
+
+    /**
+     * @param mixed $long
+     */
+    public function setLong($long)
+    {
+        $this->long = $long;
+    }
+
+}
--- src/Entity/Modele/AbstractItem.php
+++ src/Entity/Modele/AbstractItem.php
@@ -4,13 +4,17 @@ namespace App\Entity\Modele;
 
 use Doctrine\ORM\Mapping as ORM;
 
-
 /**
  * @ORM\Entity()
  * @ORM\Table(name="item")
  * @ORM\InheritanceType("JOINED")
  * @ORM\DiscriminatorColumn(name="discr", type="string")
- * @ORM\DiscriminatorMap({"personnage" = "App\Entity\Element\Personnage", "partie" = "App\Entity\Element\Partie", "evenement" = "App\Entity\Element\Evenement"})
+ * @ORM\DiscriminatorMap({
+ *     "personnage" = "App\Entity\Element\Personnage",
+ *     "partie" = "App\Entity\Element\Partie",
+ *     "evenement" = "App\Entity\Element\Evenement",
+ *     "lieu" = "App\Entity\Element\Lieu"
+ * })
  */
 abstract class AbstractItem extends AbstractElement
 {
