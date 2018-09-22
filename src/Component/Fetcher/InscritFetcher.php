aeb29a5170d99e78e2f296db2a404cd77e914bed
gaotian <gaotian@macbook-pro-de-gaotian.home>
2018-09-22 09:50:00 +0200

    MI-81:inscrit fetcher

---
+++ src/Component/Fetcher/InscritFetcher.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Component\Fetcher;
+
+use App\Entity\Concept\Inscrit;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FictionFetcher
+{
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function fetchInscrit($inscritId)
+    {
+        $inscrit = $this->em->getRepository(Inscrit::class)->findOneById($inscritId);
+
+        if (!$inscrit) {
+            throw new NotFoundHttpException(sprintf(
+                'Aucun inscrit avec l\'id "%s" n\'a été trouvé',
+                $inscritId
+            ));
+        }
+
+        return $inscrit;
+    }
+
+}
