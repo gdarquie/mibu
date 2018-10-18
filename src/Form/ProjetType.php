4c83581c8fbd4ac3bd4eec72cd63ef46dfd19657
gdarquie <gaetan.darquie@gmail.com>
2018-10-18 14:23:50 +0200

    MI-55:création de projetIO

---
+++ src/Form/ProjetType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Component\IO\ProjetIO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('fictionId')
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => ProjetIO::class,
+            'csrf_protection' => false,
+        ));
+    }
+
+}
